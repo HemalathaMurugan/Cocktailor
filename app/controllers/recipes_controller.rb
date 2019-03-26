f674a6ed797f59ba44cec6dcb9dbdeb10698c7cc
moranja <jeffadammoran@gmail.com>
2019-03-26 14:46:49 -0500

    working on form

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,19 +3,21 @@ class RecipesController < ApplicationController
     #Create
     def new
       @recipe = Recipe.new
+      @recipe_ingredients = nil
       @glasses = Recipe.glasses
       @categories = Recipe.categories
     end
 
     def create
       if params[:commit] == "Add Ingredient"
-        @recipe = Recipe.new(recipes_params(:name, :description, :instructions, :glass_type, :category))
+        @recipe = Recipe.create(recipes_params(:name, :description, :instructions, :glass_type, :category))
+        #@recipe.id = 1
         @ingredient = Ingredient.find_or_create_by(name: recipes_params(ingredients: {})["ingredients"]["ingredient_name"])
         rec_ing = RecipeIngredient.create(recipe: @recipe, ingredient: @ingredient, amount: recipes_params(ingredients: {})["ingredients"]["amount"])
+        @recipe_ingredients = @recipe.recipe_ingredients
         @glasses = Recipe.glasses
         @categories = Recipe.categories
         byebug
-        @glasses
         render :new
       else
       end
