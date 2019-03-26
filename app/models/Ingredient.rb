a2916596fe1a5157f244e4b91e7568658b693891
moranja <jeffadammoran@gmail.com>
2019-03-26 09:47:50 -0500

    validations

--- app/models/Ingredient.rb
+++ app/models/Ingredient.rb
@@ -1,3 +1,4 @@
 class Ingredient < ActiveRecord::Base
+  validates :name, uniqueness: true
 
 end
--- app/models/Recipe.rb
+++ app/models/Recipe.rb
@@ -1,4 +1,16 @@
 class Recipe < ActiveRecord::Base
+  validates :name, uniqueness: true
+  before_save :titleize_glasses, :fix_nil_in_categories
+
+  def titleize_glasses
+    self.glass_type = self.glass_type.titleize
+  end
+
+  def fix_nil_in_categories
+    if self.category == nil
+      self.category = 'Uncategorized'
+    end
+  end
 
   def self.glasses
     glasses_groups = Recipe.all.group_by{|r| r.glass_type}
@@ -7,8 +19,7 @@ class Recipe < ActiveRecord::Base
 
   def self.categories
     category_groups = Recipe.all.group_by{|r| r.category}
-    categories = category_groups.map{|k,v| v.first}.#sort_by{|r| r.category} this sort won't work because of nil in category
+    categories = category_groups.map{|k,v| v.first}.sort_by{|r| r.category}
   end
-
 
 end
