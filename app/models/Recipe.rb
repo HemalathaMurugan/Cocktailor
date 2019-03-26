f9877ec53b290b34686436c0dfa7ebac4a768f51
moranja <jeffadammoran@gmail.com>
2019-03-26 09:16:50 -0500

    fixing json

--- app/models/Recipe.rb
+++ app/models/Recipe.rb
@@ -2,12 +2,12 @@ class Recipe < ActiveRecord::Base
 
   def self.glasses
     glasses_groups = Recipe.all.group_by{|r| r.glass_type}
-    glasses = glasses_groups.map{|k,v| v.first}
+    glasses = glasses_groups.map{|k,v| v.first}.sort_by{|r| r.glass_type}
   end
 
   def self.categories
     category_groups = Recipe.all.group_by{|r| r.category}
-    categories = category_groups.map{|k,v| v.first}
+    categories = category_groups.map{|k,v| v.first}.#sort_by{|r| r.category} this sort won't work because of nil in category
   end
 
 
