fdee3f34f1588815407c24e16a16af5309c2229f
moranja <jeffadammoran@gmail.com>
2019-03-26 09:08:23 -0500

    fixed collection_select

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,6 +2,8 @@ class RecipesController < ApplicationController
 
     def new
         @recipe = Recipe.new
+        @glasses = Recipe.glasses
+        @categories = Recipe.categories
 
 
     end
--- app/models/Recipe.rb
+++ app/models/Recipe.rb
@@ -1,3 +1,14 @@
 class Recipe < ActiveRecord::Base
 
+  def self.glasses
+    glasses_groups = Recipe.all.group_by{|r| r.glass_type}
+    glasses = glasses_groups.map{|k,v| v.first}
+  end
+
+  def self.categories
+    category_groups = Recipe.all.group_by{|r| r.category}
+    categories = category_groups.map{|k,v| v.first}
+  end
+
+
 end
