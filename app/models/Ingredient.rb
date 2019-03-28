63be333e740fdc1f3fce24011e994c09e732f32d
moranja <jeffadammoran@gmail.com>
2019-03-28 14:23:54 -0500

    think recipe forms working now

--- app/models/Ingredient.rb
+++ app/models/Ingredient.rb
@@ -3,12 +3,16 @@ class Ingredient < ActiveRecord::Base
   has_many :recipes, through: :recipe_ingredients
   has_many :user_ingredients
   has_many :users, through: :user_ingredients
-  
+
   validates :name, uniqueness: true
 
 
-  def amount
-    self.recipe_ingredients.first ? self.recipe_ingredients.first.amount : nil
+  def amount(recipe: recipe)
+    byebug
+    if self.recipe_ingredients.first
+      rec_ing = self.recipe_ingredients.select {|r_i| r_i.recipe == recipe}
+      rec_ing.amount
+    end
   end
 
   def amount=(recipe: recipe, amount: amount)
