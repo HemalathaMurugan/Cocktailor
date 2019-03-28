f4f94327672779edf879708455989111c1f1c321
moranja <jeffadammoran@gmail.com>
2019-03-28 15:57:28 -0500

    fixed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,12 @@ recipes.each do |recipe|
   new_recipe = Recipe.create(name: recipe["name"], glass_type: recipe["glass"], category: recipe["category"], instructions: recipe["preparation"])
   recipe["ingredients"].each do |ingredient|
     if ingredient["ingredient"] != nil
-      recipe_ing = Ingredient.find_or_create_by(name: ingredient["ingredient"], ingredient_type: "Main ingredient")
+      if ingredient["label"] != nil
+        name = ingredient["ingredient"] + " - (#{ingredient["label"]})"
+      else
+        name = ingredient["ingredient"]
+      end
+      recipe_ing = Ingredient.find_or_create_by(name: name, ingredient_type: "Main ingredient")
       RecipeIngredient.create(recipe_id: new_recipe.id, ingredient_id: recipe_ing.id, amount: ingredient["amount"])
     elsif ingredient["special"] != nil
       recipe_ing = Ingredient.find_or_create_by(name: ingredient["special"], ingredient_type: "Special ingredient")
